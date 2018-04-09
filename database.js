25ac0d51aba9f03f1db6df3a2fab2a3b4b854083
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-09 15:57:49 -0500

    Added database to js, started on for in loop

--- database.js
+++ database.js
@@ -1,3 +1,4 @@
+// In your JavaScript file, load your home inventory from local storage.
 // Access Data from local storage
 const loadDatabase = function (localStorageKey) {
     // Get the string version of the database
@@ -7,3 +8,13 @@ const loadDatabase = function (localStorageKey) {
     return JSON.parse(databaseString)
 }
 
+const loadedDatabase = loadDatabase("Home Inventory")
+console.log(loadedDatabase);
+// Iterate over every data set in the database and create a <section> component.
+let dataIteration = ""
+for(let key in loadedDatabase) {
+    loadedDatabase[i] 
+
+// That component itself should contain three <p> components. One for the name, location, and description.
+// Attach the p components as children of the section.
+// Attach the section as a child of the article.
