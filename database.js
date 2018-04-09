9f9c86864fbf89d0668c06936b4dad20a3aa2e5d
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-09 15:34:11 -0500

    updated js and index.html with script

---
+++ database.js
@@ -0,0 +1,9 @@
+// Access Data from local storage
+const loadDatabase = function (localStorageKey) {
+    // Get the string version of the database
+    const databaseString = localStorage.getItem(localStorageKey)
+
+    // Use JSON.parse() to convert the string back into an object
+    return JSON.parse(databaseString)
+}
+
