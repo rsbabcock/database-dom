c86dc28f94c107fecf5a566c0f88da068bfa7cc7
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-10 08:55:32 -0500

    Added initial for in loop

--- database.js
+++ database.js
@@ -9,12 +9,24 @@ const loadDatabase = function (localStorageKey) {
 }
 
 const loadedDatabase = loadDatabase("Home Inventory")
-console.log(loadedDatabase);
+// console.log(loadedDatabase);
 // Iterate over every data set in the database and create a <section> component.
 let dataIteration = ""
-for(let key in loadedDatabase) {
-    loadedDatabase[i] 
-
+for(let type in loadedDatabase) { 
+    console.log(loadedDatabase)
+    for(let item in loadedDatabase[type]) {
+        console.log(loadedDatabase[type][item].name)
+    }
+}
 // That component itself should contain three <p> components. One for the name, location, and description.
 // Attach the p components as children of the section.
 // Attach the section as a child of the article.
+
+// function dump_props(obj, obj_name) {
+//     var result = '';
+//     for (var i in obj) {
+//       result += obj_name + '.' + i + ' = ' + obj[i] + '<br>';
+//     }
+//     result += '<hr>';
+//     return result;
+//   }
